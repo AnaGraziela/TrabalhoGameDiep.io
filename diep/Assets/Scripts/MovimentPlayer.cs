e0d955fe170447db6c4fc863d8fd562e11d23f6c
filipin04 <123595764+filipin04@users.noreply.github.com>
2024-02-09 18:27:17 -0400

    colocando diep na pasta

---
+++ diep/Assets/Scripts/MovimentPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentPlayer : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Moviment()
+    {
+
+    }
+}
